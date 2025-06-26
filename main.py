6a7476c2c537feb4d6feb6a8edcf3cc44b4981fc
MhayhemDev <mhayhemdev@gmail.com>
2025-06-26 17:57:33 +0100

    updated create_player

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from player_class import Player as P
 import random
 from time import sleep
 
@@ -37,13 +38,16 @@ def create_player(choice):
         player = input()
         sleep(1)
         print(f"Jugador creado: {player}")
-        return player
+        player = P(player, 0)
     else:
         print("introduzca el nombre de los jugadores:")
         print("Jugador 1:")
         player_1 = input()
+        player_1 = P(player_1, 0)
+        sleep(1)
         print("Jugador 2:")
         player_2 = input()
+        player_2 = P(player_2, 0)
         sleep(1)
         print(f"Jugadorescreados: {player_1} vs {player_2}")
         return player_1, player_2
